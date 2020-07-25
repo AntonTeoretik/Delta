7481b92b83625cd2ca7fb5af1811acd0ceb0ba34
Alisa1543 <alisa.zikeeva@gmail.com>
2020-07-25 19:57:05 +0300

    add Magnetic.hs

---
+++ DeltaProject/src/Magnetic.hs
@@ -0,0 +1,20 @@
+module Magnetic where
+
+data Circuit
+
+
+getMagneticField :: Circuit -> Point -> Vector
+getMagneticField = undefined
+
+getMagneticField :: [Circuit] -> Point -> Vector
+getMagneticField = undefined
+
+circuitFromFunction :: Int -> (Double -> Point) -> Circuit
+circuitFromFunction = undefined
+
+
+
+
+
+
+
