245c472aea684784fbbd106a26e9a7a7953e3de9
Alisa1543 <alisa.zikeeva@gmail.com>
2020-07-26 20:04:28 +0300

    add Magnetic.hs

--- DeltaProject/src/Magnetic.hs
+++ DeltaProject/src/Magnetic.hs
@@ -1,20 +1,27 @@
 module Magnetic where
 
-data Circuit
+import Algebra	
 
+data Circuit = Circuit Double Point
+  deriving (Show, Eq, Generic, NFData)
 
-getMagneticField :: Circuit -> Point -> Vector
-getMagneticField = undefined
+magicConst :: Double
+magicConst = (4 * pi * 10 ** (-7)) / (4 * pi)
 
-getMagneticField :: [Circuit] -> Point -> Vector
-getMagneticField = undefined
+--getMagneticField :: Circuit -> Point -> Vector
+--getMagneticField ( Circuit (a) (Point (x) (y) (z)) ) = (magicConst / 4) + ((a * () ()))
+
+getMagneticFieldSystem :: [Circuit] -> Point -> Vector
+getMagneticFieldSystem = undefined
 
 circuitFromFunction :: Int -> (Double -> Point) -> Circuit
-circuitFromFunction = undefined
+circuitFromFunction n c = Circuit (n) (c n)
 
+circle :: Double -> Point
+circle x = Point (sin x) (cos x) 0
 
-
-
+getElementaryInduction :: Point -> Point -> Point -> Vector
+getElementaryInduction (Point x y z) (Point x1 y1 z1) (Point x2 y2 z2) = ((-->) (Point x y z) (Point x2 y2 z2)) <#> (-->) ((Point x y z) (Point x1 y1 z1))
 
 
 
