6166e0c0e4a89a380990f8c726459c2e45a02235
Dimitri Ter-Saakov <terdimitri@gmail.com>
2020-07-26 13:36:10 -0400

    Revert "chanche package.yaml"
    This reverts commit 91995abf0f9ab7e06ff03412b31d709e1d0bc084.

--- DeltaProject/src/Random.hs
+++ DeltaProject/src/Random.hs
@@ -1,24 +1,9 @@
 module Random where 
-import System.Random	
 import Algebra
 
 
 generatePointsInCube :: Int -> Double -> [Point]
-generatePointsInCube = randomRs mkStdGen x (1, 10)
-
---randomRs mkStdGen x (1, 10)
-
-
-
---randomRs :: RandomGen g => (Int, Int) -> g -> [Int]
-
---mkStdGen :: Int -> StdGen
-
-
-
+generatePointsInCube = undefined
 
 generatePointsInSphere :: Int -> Double -> [Point]
 generatePointsInSphere = undefined
-
-generatePointsOnSphere :: Int -> Double -> [Point]
-generatePointsOnSphere = undefined
