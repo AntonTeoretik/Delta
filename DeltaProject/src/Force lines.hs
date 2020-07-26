9d80e564ceb21b577ee3826f124eb9898e4be070
SonyaTreshchova <sontre@yandex.ru>
2020-07-26 19:05:19 +0300

    Type ForceLines

--- DeltaProject/src/Force lines.hs
+++ DeltaProject/src/Force lines.hs
@@ -1,1 +1,31 @@
-module ForseLines where
+module ForceLines where
+
+data ForceLines = ForceLines [[Point]]
+derivng (Show, Eq)
+
+buildForceLineFromPoint :: Double -> (Point -> Vector) -> Point -> [Point]
+buildForceLineFromPoint a (Vector x y z) (Point k l m) = (Point k l m) : map (.->) buildForceLineFromPoint (Vector x1 y1 z1) 
+                               where (Vector x1 y1 z1) = a *. (normalize Vector x y z) 
+
+buildFroVecField :: Double -> (Point -> Vector) -> ForceLines
+buildFroVecField = underfined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
