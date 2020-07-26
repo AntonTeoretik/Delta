91995abf0f9ab7e06ff03412b31d709e1d0bc084
Ilja-D <mathefreak3.0@hotmail.com>
2020-07-26 19:27:32 +0200

    chanche package.yaml

--- DeltaProject/src/Random.hs
+++ DeltaProject/src/Random.hs
@@ -1,9 +1,24 @@
 module Random where 
+import System.Random	
 import Algebra
 
 
 generatePointsInCube :: Int -> Double -> [Point]
-generatePointsInCube = undefined
+generatePointsInCube = randomRs mkStdGen x (1, 10)
+
+--randomRs mkStdGen x (1, 10)
+
+
+
+--randomRs :: RandomGen g => (Int, Int) -> g -> [Int]
+
+--mkStdGen :: Int -> StdGen
+
+
+
 
 generatePointsInSphere :: Int -> Double -> [Point]
 generatePointsInSphere = undefined
+
+generatePointsOnSphere :: Int -> Double -> [Point]
+generatePointsOnSphere = undefined
