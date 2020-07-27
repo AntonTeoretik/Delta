dfa68a60e454e3dd972091f933be4344d4518cc8
SonyaTreshchova <sontre@yandex.ru>
2020-07-27 18:43:34 +0300

    buildForceLineFromPoint

--- DeltaProject/src/ForceLines.hs
+++ DeltaProject/src/ForceLines.hs
@@ -7,7 +7,8 @@ derivng (Show, Eq)
 
 buildForceLineFromPoint :: Double -> (Point -> Vector) -> Point -> [Point]
 buildForceLineFromPoint a f x = x : zipWith f' buildForceLineFromPoint (tail buildForceLineFromPoint)
-                               where f' = if a < veclength $ f x then .-> $ a *. $ f x else  .-> $ f x 
+                               where f':: Point -> Point
+							         f' = if a < veclength $ f x then .-> $ a *. $ normalize $ f x else  .-> $ f x 
 
 buildFroVecField :: Double -> (Point -> Vector) -> ForceLines
 buildFroVecField = underfined
