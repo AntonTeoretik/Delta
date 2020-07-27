f1888af81968a6747939726d088e0492e95bc260
Ilja-D <mathefreak3.0@hotmail.com>
2020-07-27 17:55:02 +0200

    make randomNumber

--- DeltaProject/src/Random.hs
+++ DeltaProject/src/Random.hs
@@ -1,9 +1,25 @@
 module Random where 
+import System.Random	
 import Algebra
 
 
 generatePointsInCube :: Int -> Double -> [Point]
 generatePointsInCube = undefined
 
+--randomRs mkStdGen x (1, 10)
+
+randomNumber :: Int -> [Double]
+randomNumber x = randomRs (1, 10) (mkStdGen x)
+
+--randomRs :: RandomGen g => (Int, Int) -> g -> [Int]
+
+--mkStdGen :: Int -> StdGen
+
+
+
+
 generatePointsInSphere :: Int -> Double -> [Point]
 generatePointsInSphere = undefined
+
+generatePointsOnSphere :: Int -> Double -> [Point]
+generatePointsOnSphere = undefined 
