b3ef8c016b23bd84e55f2c35fd032357f515b525
Alisa1543 <alisa.zikeeva@gmail.com>
2020-07-27 18:24:26 +0300

    change circle getElementaryInduction circuitFromFunction

--- DeltaProject/src/Magnetic.hs
+++ DeltaProject/src/Magnetic.hs
@@ -10,19 +10,18 @@ magicConst = (4 * pi * 10 ** (-7)) / (4 * pi)
 
 --getMagneticField :: Circuit -> Point -> Vector
 --getMagneticField ( Circuit (a) (Point (x) (y) (z)) ) = (magicConst / 4) + ((a * () ()))
--- hhhhhhh
 
 getMagneticFieldSystem :: [Circuit] -> Point -> Vector
 getMagneticFieldSystem = undefined
 
-circuitFromFunction :: Int -> (Double -> Point) -> Circuit
-circuitFromFunction n c = Circuit (n) (c n)
+circuitFromFunction :: Int -> Double -> (Double -> Point) -> Circuit
+circuitFromFunction n z c = Circuit (n) (c n)
 
-circle :: Double -> Point
-circle x = Point (sin x) (cos x) 0
+circle :: Double -> [Point]
+circle x = Point (sin x) (cos x) 0 : []
 
 getElementaryInduction :: Point -> Point -> Point -> Vector
-getElementaryInduction (Point x y z) (Point x1 y1 z1) (Point x2 y2 z2) = ((-->) (Point x y z) (Point x2 y2 z2)) <#> (-->) ((Point x y z) (Point x1 y1 z1))
+getElementaryInduction r0 r 1 = ((-->) r r1) <#> ((-->) r r0)
 
 
 
