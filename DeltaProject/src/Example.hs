47f7d7029b33782cf21b90eb9649a3d6ebfd8f74
Dimitri Ter-Saakov <terdimitri@gmail.com>
2020-07-28 12:07:09 -0400

    add points populating the unit cube

--- DeltaProject/src/Example.hs
+++ DeltaProject/src/Example.hs
@@ -1,5 +1,6 @@
 module Example
     ( simpleField
+    , points
     ) where
 
 import Graphics
@@ -10,6 +11,9 @@ import PointsForRendering
 simpleField :: A.Point -> A.Vector
 simpleField (A.Point x y z) = (A.Vector x y z) <#> (A.Vector 1 0 0)
 
+points = [(A.Point x y z) | x <- [0.02, 0.04 .. 1]
+                          , y <- [0.02, 0.04 .. 1]
+                          , z <- [0.02, 0.04 .. 1]]
 
 --main = renderInWindow (displayVector ((A.Point 0.1 0.3 0.5), (A.Vector 0.4 0.4 0.4)))
 {- main = createWindow (displayVecField simpleField [(A.Point 0.5 0.1 0)
