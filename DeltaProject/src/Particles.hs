ca4c77e81264b187dffbc476336a8867f698b265
YashaB-eng <pronto.yasha@gmail.com>
2020-07-28 23:16:18 +0700

    change Particles.hs

--- DeltaProject/src/Particles.hs
+++ DeltaProject/src/Particles.hs
@@ -8,18 +8,18 @@ data Particle = Particle {Point Vector lifetime :: Integer, mass :: Double, elec
 data SystemOfParticles = SystemOfParticles { listOfParticles :: [Particle], radiusOfRegion :: Double}
 
 evaluateParticle :: Double -> Vector -> Particle -> Particle
-evaluateParticle k (Vector x y z) (Particle (Point x1 y1 z1) (Vector x2 y2 z2) lifetime t, mass m, electricCharge q) = Particle (Point x1 y1 z1 .-> Vector x y z)  (Vector ((k *. (Vector x y z /. mass m)) <+> (Vector x2 y2 z2)))  lifetime (t - 1) mass m electricCharge q  
+evaluateParticle k x (Particle {x1 x2 lifetime t, mass m, electricCharge q}) = Particle (x1 .-> x)  (Vector ((k *. (x /. m)) <+> x2))  lifetime (t - 1) mass m electricCharge q  
 
 evaluateSystemOfParticles :: Double -> (Point -> Vector) -> (Point -> Vector) -> SystemParticles -> SystemParticles
-evaluateSystemOfParticles k getMagneticForce (Vector (f x y z)) (Particle (Point x1 y1 z1) (Vector x2 y2 z2) lifetime t mass m electricCharge q) getElectricForce (Vector (f x' y' z')) (Particle (Point x1' y1' z1') (Vector x2' y2' z2') lifetime t' mass
- m' electricCharge q') (SystemOfParticles listOfParticles a radiusOfRegion b) = |
-                                                                                | 
+evaluateSystemOfParticles k getMagneticForce (Vector (f)) (Particle {x1 x2 lifetime t, mass m, electricCharge q}) (getElectricForce (Vector (f')) (Particle {x1' x2' lifetime t', mass m', electricCharge q'})
+(SystemOfParticles {listOfParticles (a : as), radiusOfRegion b}) = | a
+                                                                   | 
 
 addParticleToSystem :: Particle -> SystemOfParticles -> SystemOfParticles
-addParticleToSystem (Particle (Point x1 y1 z1) (Vector x2 y2 z2) lifetime t mass m electricCharge q)  (SystemOfParticles listOfParticles a radiusOfRegion b) = SystemOfParticles ((Particle (Point x1 y1 z1) (Vector x2 y2 z2) lifetime t mass m electricCharge q) : a  (b) )  
+addParticleToSystem (Particle {x1 x2 lifetime t, mass m, electricCharge q})  (SystemOfParticles {listOfParticles a, radiusOfRegion b}) = SystemOfParticles ((Particle {x1 x2 lifetime t, mass m, electricCharge q}) : a  (b) )  
 
 getMagneticForce :: (Point -> Vector) -> Particle -> Vector 
-getMagneticForce (Vector (f x y z)) (Particle (Point x1 y1 z1) (Vector x2 y2 z2) lifetime t mass m electricCharge q) = Vector $ q *. $ (Vector x2 y2 z2) <*> (Vector $ f x y z)
+getMagneticForce (Vector (f)) (Particle {x1 x2 lifetime t, mass m, electricCharge q}) = Vector $ q *. $ x2 <*> (Vector $ f)
 
 getElectricForce :: (Point -> Vector) -> Particle -> Vector
-getElectricForce (Vector (f x y z)) (Particle (Point x1 y1 z1) (Vector x2 y2 z2) lifetime t mass m electricCharge q) = Vector $ q *. (Vector (f x y z)) 
+getElectricForce (Vector (f)) (Particle {x1 x2 lifetime t, mass m, electricCharge q}) = Vector $ q *. (Vector (f)) 
