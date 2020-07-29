08e168ee3e4b1a68e646e54172a3e9f5567ae343
YashaB-eng <pronto.yasha@gmail.com>
2020-07-30 00:57:36 +0700

    finished file Particles.hs

--- DeltaProject/src/Particles.hs
+++ DeltaProject/src/Particles.hs
@@ -9,11 +9,15 @@ data Particle = Particle {position Point, velocity Vector, lifetime :: Integer, 
 data SystemOfParticles = SystemOfParticles { listOfParticles :: [Particle], radiusOfRegion :: Double}
 
 evaluateParticle :: Double -> Vector -> Particle -> Particle
-evaluateParticle k f (Particle x1 v t m q) = Particle ((x1 .-> f) ((k *. (f /. m)) <+> v) (t - 1) m q) 
+evaluateParticle k f (Particle x1 v t m q) = Particle (x1 .-> k *. v ) ((k *. (f /. m)) <+> v) (t - 1) m q
+
+moveByField :: Double -> (Point -> Vector) -> (Point -> Vector) -> Particle -> Particle
+moveByField k f f' p = evaluateParticle k force p 
+                                        where 
+                                          force = (getElectricForce f p) <+> (getMagneticForce f' p)  
 
 evaluateSystemOfParticles :: Double -> (Point -> Vector) -> (Point -> Vector) -> SystemParticles -> SystemParticles
-evaluateSystemOfParticles k f f' (SystemOfParticles a r) = map (if Particle {lifetime <= 0} then filter Particle {lifetime <= 0} (a) else evaluateParticle k (f <+> f') ) (a) 
-                                                                  
+evaluateSystemOfParticles k f f' (SystemOfParticles a r) = filter (\(Particle _ _ l _ _ ) -> l > 0) (map (moveByField k f f' ) (a))
 
 addParticleToSystem :: Particle -> SystemOfParticles -> SystemOfParticles
 addParticleToSystem particle (SystemOfParticles a r) = SystemOfParticles (particle : a) r  
