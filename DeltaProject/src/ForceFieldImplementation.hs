f543c3ba94a4d8c0955592e0f2f6091b8cacfa96
asdfghjkl268 <68605507+asdfghjkl268@users.noreply.github.com>
2020-07-29 12:31:36 +0300

    Update ForceFieldImplementation.hs

--- DeltaProject/src/ForceFieldImplementation.hs
+++ DeltaProject/src/ForceFieldImplementation.hs
@@ -3,7 +3,7 @@ data StaticElectricParticle = StaticElectricParticle {position :: Point, charge 
   deriving (Eq, Generic, NFData, Show)
 
 getElectricField :: StaticElectricParticle -> Point -> Vector -- 
-getElectricField particle p = particle * ((veclenght(p (-->) (1, 1, 1)))^3)/((9*(10^9))*p*1*(p (-->) (1, 1, 1))) --
+getElectricField particle p = particle * ((veclenght(p (-->) (1, 1, 1)))^3)/((9*(10^9))*p*1) --
 
 getElectricFieldSystem :: [StaticElectricParticle] -> Point -> Vector -- 
 
