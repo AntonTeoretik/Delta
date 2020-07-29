fb4d2b364d9721158ae9481458b588be92960e2a
SonyaTreshchova <sontre@yandex.ru>
2020-07-29 19:45:48 +0300

    buildFromVecField

--- DeltaProject/src/ForceLines.hs
+++ DeltaProject/src/ForceLines.hs
@@ -1,6 +1,7 @@
 module ForceLines where
 
 import Algebra
+-- import Random (пока комментарий)
 
 data ForceLines = ForceLines [[Point]]
      deriving (Show, Eq)
@@ -11,8 +12,16 @@ buildForceLineFromPoint a f x = x : buildForceLineFromPoint a f (nextPoint a f x
 nextPoint :: Double -> (Point -> Vector) -> Point -> Point
 nextPoint a f x = x .-> (a *. (normalize (f x))) 
 
-buildFroVecField :: Double -> (Point -> Vector) -> ForceLines
-buildFroVecField = undefined
+buildFromVecField :: Double -> Double -> Int -> Int -> (Point -> Vector) -> ForceLines
+buildFromVecField x a b i f = map (buildForceLineFromPoint a f) (take b $ generatePointsInCube i x)   
+
+-- x - сторона куба 
+-- а - расстояние между т. на сил. лин.
+-- b - кол-во сил. лин.
+-- i - управляет generatePointsInCube
+
+generatePointsInCube :: Int -> Double -> [Point] -- пока не подключен модуль Random
+generatePointsInCube = undefined
 
 
 
