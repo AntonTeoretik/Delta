152cd0181c3f3e21482a0374b1299908b77a3c7a
Dimitri Ter-Saakov <terdimitri@gmail.com>
2020-07-30 12:03:42 -0400

    Clean Magnetic.hs

--- DeltaProject/src/Magnetic.hs
+++ DeltaProject/src/Magnetic.hs
@@ -1,37 +1,28 @@
-module Magnetic where
+module Magnetic
+  ( Circuit(..)
+  , getMagneticField
+  , circuitFromFunction
+  , circle
+  ) where
 
-import Algebra	
+import Algebra
 
 data Circuit = Circuit Double [Point]
   deriving (Show, Eq)
-
-magicConst :: Double
-magicConst = (4 * pi * 10 ** (-7)) / (4 * pi)
 
 getMagneticField :: Circuit -> Point -> Vector
 getMagneticField (Circuit t r2 ) p = (*.) t (foldl (<+>) zero list)
   where
     list = zipWith (getElementaryInduction p) r2 ((tail r2) ++ [head r2])
--- list :: [Vector] 
--- () :: ...-> Vector
--- [] :: ...-> Point
---
-
 
 getMagneticFieldSystem :: [Circuit] -> Point -> Vector
 getMagneticFieldSystem list p = foldl (<+>) zero (map (\t -> getMagneticField t p) list)
 
-
-
 circuitFromFunction :: Int -> Double -> (Double -> Point) -> Circuit
 circuitFromFunction n z c = Circuit (z) (map (c) [0,(2*pi / realToFrac(n))..(2*pi)])
 
-
 circle :: Double -> Point
-circle x = Point (sin x) (cos x) 0 
+circle x = Point (sin x) (cos x) 0
 
 getElementaryInduction :: Point -> Point -> Point -> Vector
-getElementaryInduction r0 r r1 =  (*.) (1 / ((veclength ((-->) r r0)) ** 2)) (((-->) r r1) <#> (normalize ((-->) r r0)))  
-
-
-
+getElementaryInduction r0 r r1 = (*.) (1 / ((veclength ((-->) r r0)) ** 2)) (((-->) r r1) <#> (normalize ((-->) r r0)))
