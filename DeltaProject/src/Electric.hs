89dfe44f6cc38dc862abea9fa8f7708e3f4affd1
asdfghjkl268 <68605507+asdfghjkl268@users.noreply.github.com>
2020-07-30 22:25:15 +0300

    fixed  getElectricFied formula

--- DeltaProject/src/Electric.hs
+++ DeltaProject/src/Electric.hs
@@ -1,12 +1,15 @@
 module Electric where
+
+import Algebra
+
 data StaticElectricParticle = StaticElectricParticle {position :: Point, charge :: Double}
   deriving (Eq, Show)
 
 getElectricField :: StaticElectricParticle -> Point -> Vector -- 
-getElectricField particle p = particle * ((veclenght(p (-->) (1, 1, 1)))^3)/((9*(10^9))*p*1) --
+getElectricField particle p = (*.)((9*10^9)*(charge particle)/(veclength ((-->) (position particle) p))^3)((-->) (position particle) p) --
 
 getElectricFieldSystem :: [StaticElectricParticle] -> Point -> Vector 
-getElectricFieldSystem particles p= map(getElectricField) particles p
+getElectricFieldSystem particles p= undefined
 
 getSystemFromFuction :: (Double, Double) -> (Point, Double) -> [StaticElectricParticle] --
 getSystemFromFuction = undefined
