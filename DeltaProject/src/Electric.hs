6bd50c117523e10f3fc8644cb1f90c416a491a03
asdfghjkl268 <68605507+asdfghjkl268@users.noreply.github.com>
2020-07-30 18:47:17 +0300

    Update Electric.hs

--- DeltaProject/src/Electric.hs
+++ DeltaProject/src/Electric.hs
@@ -1,11 +1,12 @@
 module Electric where
 data StaticElectricParticle = StaticElectricParticle {position :: Point, charge :: Double}
-  deriving (Eq, Generic, NFData, Show)
+  deriving (Eq, Show)
 
 getElectricField :: StaticElectricParticle -> Point -> Vector -- 
 getElectricField particle p = particle * ((veclenght(p (-->) (1, 1, 1)))^3)/((9*(10^9))*p*1) --
 
-getElectricFieldSystem :: [StaticElectricParticle] -> Point -> Vector -- 
+getElectricFieldSystem :: [StaticElectricParticle] -> Point -> Vector 
 getElectricFieldSystem particles p= map(getElectricField) particles p
 
 getSystemFromFuction :: (Double, Double) -> (Point, Double) -> [StaticElectricParticle] --
+getSystemFromFuction = undefined
