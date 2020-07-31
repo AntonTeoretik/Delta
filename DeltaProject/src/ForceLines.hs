659a48bcfd150c85c5ce475ae70877eeeec3068d
SonyaTreshchova <sontre@yandex.ru>
2020-07-31 20:12:27 +0300

    nextPointBack

--- DeltaProject/src/ForceLines.hs
+++ DeltaProject/src/ForceLines.hs
@@ -1,20 +1,25 @@
 module ForceLines where
-
 import Algebra
-import Random 
-
+import Random
 data ForceLines = ForceLines [[Point]]
      deriving (Show, Eq)
-
-buildForceLineFromPoint :: Double -> (Point -> Vector) -> Point -> [Point]
-buildForceLineFromPoint a f x = x : buildForceLineFromPoint a f (nextPoint a f x) 
+buildForceLineFromPoint :: Double -> Int -> (Point -> Vector) -> Point -> [Point]
+buildForceLineFromPoint a b f x = back : front
+                        where back  = init $ reverse $ take b (x : buildForceLineFromPoint a f (nextPointBack a f x))
+                              front = take b (x : buildForceLineFromPoint a f (nextPoint a f x))
+-- a - расстояние между т. на сил. лин.
+-- 2*b - кол-во т. на сил. лин
+-- x - первая т.
 
 nextPoint :: Double -> (Point -> Vector) -> Point -> Point
-nextPoint a f x = x .-> (a *. (normalize (f x))) 
+nextPoint a f x = x .-> (a *. (normalize (f x)))
+
+nextPointBack :: Double -> (Point -> Vector) -> Point -> Point -- в обратную сторону
+nextPointBack a f x = x .-> (((-1) * a) *. (normalize (f x)))
 
 buildFromVecField :: Double -> Double -> Int -> Int -> (Point -> Vector) -> ForceLines
-buildFromVecField x a b i f = ForceLines $ map (buildForceLineFromPoint a f) (take b $ generatePointsInCube i x)   
--- x - сторона куба 
+buildFromVecField x a b i f = ForceLines $ map (buildForceLineFromPoint a f) (take b $ generatePointsInCube i x)
+-- x - сторона куба
 -- а - расстояние между т. на сил. лин.
 -- b - кол-во сил. лин.
 -- i - управляет generatePointsInCube
