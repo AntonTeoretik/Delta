d2d07cddbd61a7ed6ad728eeada6ca6a2a9b477e
asdfghjkl268 <68605507+asdfghjkl268@users.noreply.github.com>
2020-07-31 15:20:36 +0300

    added getElectricFieldSystem function

--- DeltaProject/src/Electric.hs
+++ DeltaProject/src/Electric.hs
@@ -9,7 +9,7 @@ getElectricField :: StaticElectricParticle -> Point -> Vector --
 getElectricField particle p = (*.)((9*10^9)*(charge particle)/(veclength ((-->) (position particle) p))^3)((-->) (position particle) p) --
 
 getElectricFieldSystem :: [StaticElectricParticle] -> Point -> Vector 
-getElectricFieldSystem particles p= undefined
+getElectricFieldSystem particles p= foldl (<+>) zero (map (\t -> getElectricField t p) particles)
 
 getSystemFromFuction :: (Double, Double) -> (Point, Double) -> [StaticElectricParticle] --
 getSystemFromFuction = undefined
